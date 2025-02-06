0ded9636f41aa16284ac37bf86bf90a6af5c009b
karivarkey <geevargheseregi78@gmail.com>
2025-02-06 18:26:08 +0530

    chore(tailwindcss):Initialized and setup nativewind

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import './../global.css'
+type Props = {}
+
+const index = (props: Props) => {
+  return (
+    <View className='bg-red-900'>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
