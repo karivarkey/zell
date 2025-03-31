c6c793f53183eac75fe68e84c4230534bcab3250
karivarkey <geevargheseregi78@gmail.com>
2025-03-31 20:56:31 +0530

    fix(constants): Update API endpoint to production URL

--- constants/constants.ts
+++ constants/constants.ts
@@ -1,2 +1,2 @@
-//export const API = "https://zell-backend-845082295063.asia-south1.run.app";
-export const API = "http://192.168.1.4:3003";
+export const API = "https://zell-backend-845082295063.asia-south1.run.app";
+//export const API = "http://192.168.1.4:3003";
